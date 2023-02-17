422776d7abfc87ca6993ad43ce92cb905574fa80
Efe Özen <111181897+efezen@users.noreply.github.com>
2023-02-17 18:32:55 +0300

    finish

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import Container from "@mui/material/Container";
+import Header from "./Component/Header/Header";
+import Definitions from "./Component/Definition/Definition";
 
 function App() {
+  const [meanings, setMeanings] = useState([]);
+  const [input, setInput] = useState("");
+  const dictionaryApiData = async () => {
+    try {
+      const data = await axios.get(
+        `https://api.dictionaryapi.dev/api/v2/entries/en/${input}`
+      );
+      setMeanings(data.data);
+    } catch {}
+  };
+console.log(meanings);
+  useEffect(() => {
+    dictionaryApiData();
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ background: "#067597", height: "100vh" }}>
+      <Container
+        maxWidth="md"
+        style={{ display: "flex", flexDirection: "column", height: "100vh" }}
+      >
+        <Header input={input} setInput={setInput} />
+        <Definitions meanings={meanings} word={input} />
+      </Container>
     </div>
   );
 }
